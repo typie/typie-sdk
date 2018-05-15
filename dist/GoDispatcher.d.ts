5d18fc4ff5d575b9bca109064ca6efbf5208c06b
Rotem Grimberg <rotemgrim@gmail.com>
2018-05-16 00:34:25 +0300

    support auto reconnect with typie

--- dist/GoDispatcher.d.ts
+++ dist/GoDispatcher.d.ts
@@ -2,8 +2,11 @@ import Packet from "./models/Packet";
 export default class GoDispatcher {
     static go: any;
     static listening: boolean;
+    private static executablePath;
     constructor(hasteExecutable: string);
     send(packet: Packet): Promise<any>;
     close(): void;
+    private startProcess();
+    private onClose();
     private register();
 }
