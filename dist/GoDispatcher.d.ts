37f15a39e27fb54f8c0d1ea62bf288d523d84e8f
rotem <rotemgrim@gmail.com>
2019-03-17 11:55:47 +0200

    emit typie listening event - compile

--- dist/GoDispatcher.d.ts
+++ dist/GoDispatcher.d.ts
@@ -1,7 +1,10 @@
+/// <reference types="node" />
+import { EventEmitter } from "events";
 import Packet from "./models/Packet";
 export default class GoDispatcher {
     static go: any;
     static listening: boolean;
+    static emitter: EventEmitter;
     private static executablePath;
     constructor(typieExecutable: string);
     send(packet: Packet): Promise<any>;
